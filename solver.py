8938e5fcccec28f0fe02c3491536a032e08b3049
vigbab <vigneshbaburaj@icloud.com>
2023-08-14 10:24:44 +0530

    added find_empty function

--- solver.py
+++ solver.py
@@ -25,3 +25,9 @@ def print_board(bo):
                 print(str(bo[i][j]) + " ", end="")
 
 print_board(board)
+
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j]==0:
+                return (i,j)
